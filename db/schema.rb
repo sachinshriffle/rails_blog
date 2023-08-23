e81c60103858ed310516fdc3dc2160ad76493669
sachin <sachinp@shriffle.com>
2023-08-23 19:19:47 +0530

    create user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_134443) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_062542) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_134443) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
