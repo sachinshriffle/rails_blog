38cb72285a7d3d147a2a571d5b08a4e52eef6653
sachin <sachinp@shriffle.com>
2023-08-23 19:19:22 +0530

    run a router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-	root "articles#index"
-	resources :articles do
-		resources :comments
-	end
+	# root "articles#index"
+	# resources :articles do
+	# 	resources :comments
+	# end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "/users", to: "users#index"
+  # post "/users", to: "users#create"
+  # patch "/users/:id", to: "users#update"
+  # get "/users/:id", to: "users#show"
+  # delete "/users/:id", to: "users#delete"
+  # put "/users/:id", to: "users#update"
+  # get "users/download_pdf/:id", to: "users#download_pdf"
+  # get "/users/stream", to: "users#stream"
+  resources :users 
 end
